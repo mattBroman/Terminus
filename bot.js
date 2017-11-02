f439ed870bba60b92c9158326518fdedac68171f
Matthew Broman <broman334@tamu.edu>
2017-11-01 20:06:51 -0500

    added a )

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ function respond() {
       botRegex = /haha/,
       nickToNicc = / \.*nick\.* /;
 
-  if(request.text && botRegex.test(request.text)) {
+  if(request.text && botRegex.test(request.text))) {
     this.res.writeHead(200);
     postMessage(1);
     this.res.end();
