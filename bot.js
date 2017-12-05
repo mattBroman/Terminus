d5e4db0abb92f0447bdc685bedb05a9f0e011f6f
James Aubin <jaubin9422@localhost.localdomain>
2017-12-05 11:02:43 -0600

    i main chase in melee

--- bot.js
+++ bot.js
@@ -24,7 +24,8 @@ var chasePastas = [
 'Okay, fine, but that\'s not my problem right now',
 'Thanks for giving us a fucked API.',
 'Forgive me for assuming that you\'d at least write a "getSize" function correctly. Apparently you can\'t even do that right.',
-'Alright, I apologize to everyone for what happened earlier.\n\nBut show\'s over, let\'s all agree to be civil and keep this groupme as simply a place where we can ask each other for help and not worry about the formalities.\n\nAt this point, I\'d like to ask everyone that isn\'t in this class to please leave the groupme.'
+'Alright, I apologize to everyone for what happened earlier.\n\nBut show\'s over, let\'s all agree to be civil and keep this groupme as simply a place where we can ask each other for help and not worry about the formalities.\n\nAt this point, I\'d like to ask everyone that isn\'t in this class to please leave the groupme.',
+'homework due at 11:59 pm\nStarts homework at 11:30 pm\nTurns homework in at 11:59 pm'
 ];
 
 var nikkPastas = [
