57f4f729ccaedfe3445f27c2c8bd369b71f5ac0c
mattBroman <matthew.broman@yahoo.com>
2018-04-09 13:51:04 -0500

    yup

--- bot.js
+++ bot.js
@@ -1,24 +1,28 @@
-const HTTPS = require('https');
-const Chase = require('./commands/chase.js');
-const Jc = require('./commands/jc.js');
-const Term = require('./commands/copypastas.js');
-const Nikk = require('./commands/nikk.js');
-const Haha = require('./commands/haha.js');
-const Nicc = require('./commands/nicc.js');
-const FastSqr = require('./commands/wtf.js');
-const OwO = require('./commands/0w0.js');
-const Buldge = require('./commands/buldge.js');
-const Layluh = require('./commands/layluh.js');
-const Lmao = require('./commands/lmao.js');
-const Lorn = require('./commands/lorn.js');
-const Malloc = require('./commands/malloc.js');
-const Nanomachines = require('./commands/nanomachines.js');
-const Np = require('./commands/np.js');
-const Oclelote = require('./commands/ocelote.js');
-const RNH = require('./commands/RNH.js');
-const Salami = require('./commands/salami.js');
-const WTF = require('./commands/wtf.js');
-const Bomb = require('./commands/bomb.js');
+try {
+  const HTTPS = require('https');
+  const Chase = require('./commands/chase.js');
+  const Jc = require('./commands/jc.js');
+  const Term = require('./commands/copypastas.js');
+  const Nikk = require('./commands/nikk.js');
+  const Haha = require('./commands/haha.js');
+  const Nicc = require('./commands/nicc.js');
+  const FastSqr = require('./commands/wtf.js');
+  const OwO = require('./commands/0w0.js');
+  const Buldge = require('./commands/buldge.js');
+  const Layluh = require('./commands/layluh.js');
+  const Lmao = require('./commands/lmao.js');
+  const Lorn = require('./commands/lorn.js');
+  const Malloc = require('./commands/malloc.js');
+  const Nanomachines = require('./commands/nanomachines.js');
+  const Np = require('./commands/np.js');
+  const Oclelote = require('./commands/ocelote.js');
+  const RNH = require('./commands/RNH.js');
+  const Salami = require('./commands/salami.js');
+  const WTF = require('./commands/wtf.js');
+  const Bomb = require('./commands/bomb.js');
+} catch(err) {
+  console.log(err)
+}
 
 
 
